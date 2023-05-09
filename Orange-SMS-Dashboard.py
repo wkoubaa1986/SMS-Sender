7a63bac17d159abd25cf8c32b096dbcf97cbd691
wkoubaa1986 <56608278+wkoubaa1986@users.noreply.github.com>
2023-05-09 14:34:09 +0100

    augmenter le nombre de la liste a 100 contacts

--- Orange-SMS-Dashboard.py
+++ Orange-SMS-Dashboard.py
@@ -326,9 +326,7 @@ def create_list(SelectedData,Ancienne_Liste):
         st.write('First Name found')
         Client=list(Ancienne_Liste[['First Name','Last Name']].apply(lambda row: ' '.join(row.values.astype(str)).replace('nan','').replace(' ','').replace('None','').lower(), axis=1))
         SelectedData=SelectedData.sort_values(by = 'Dernier entretien')
-        st.write(Client)
         SelectedData['Client']=SelectedData[['First Name','Last Name']].apply(lambda row: ' '.join(row.values.astype(str)).replace('nan','').replace(' ','').replace('None','').lower(), axis=1)
-        st.write(SelectedData['Client'])
         SelectedData['Exist']=SelectedData['Client'].apply(lambda x: x in Client)
         SelectedData['Client']=SelectedData[['First Name','Last Name']].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
         SelectedData=SelectedData[SelectedData['Exist']==False]
